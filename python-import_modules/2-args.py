173dc9e5ef74fed3d8ca50c5715e408ccca7d9f2
Ayxan <ayxanabbas.32@example.com>
2025-06-16 11:06:19 +0000

    4959

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]  # argv[0] proqramın adı, onu atırıq
+    count = len(argv)
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print(f"{count} arguments:")
+
+    for i in range(count):
+        print(f"{i + 1}: {argv[i]}")
