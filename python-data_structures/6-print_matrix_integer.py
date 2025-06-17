5fadaf04f5305594ec21d998b8ddcb2ede3a549c
Ayxan <ayxanabbas.32@example.com>
2025-06-17 15:16:38 +0000

    893475

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, item in enumerate(row):
-            if i != len(row) - 1:
-                print("{:d} ".format(item), end="")
-            else:
-                print("{:d}".format(item))
+        if not row:  # boş sətir varsa, çap etmə
+            continue
+        print(" ".join("{:d}".format(i) for i in row))
+
