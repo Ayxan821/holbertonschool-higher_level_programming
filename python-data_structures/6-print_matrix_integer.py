bb91c48632447ac5f3933515e670080db3985d55
Ayxan <ayxanabbas@example.com>
2025-06-19 09:48:06 +0000

    8645643

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    # Əgər matrix boşdursa və ya matrix tək boş siyahıdan ibarətdirsə,
-    # heç nə çap etmirik
-    if matrix == [[]] or matrix == []:
-        return
-
     for row in matrix:
-        # boş satır varsa, çap etmə
-        if not row:
-            continue
-        print(" ".join("{:d}".format(i) for i in row))
+        print(" ".join("{:d}".format(num) for num in row))
