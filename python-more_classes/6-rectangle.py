df45758512a5412800d4873754ba40687a805166
Ayxan <ayxanabbas.32@example.com>
2025-06-24 15:33:25 +0000

    55

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -52,7 +52,8 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return 2 * (self.width +
+                    self.height)
 
     def __str__(self):
         """Return string representation of the rectangle with '#' characters.
