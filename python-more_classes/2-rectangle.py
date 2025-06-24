c9a083f560fe1de9cf22f34c2e7685f0c64396f8
Ayxan <ayxanabbas.32@example.com>
2025-06-24 15:19:18 +0000

    89485

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -6,18 +6,15 @@ class Rectangle:
     """Represents a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize rectangle with optional width and height."""
-        self.width = width    # Setter ilə yoxlanacaq
-        self.height = height  # Setter ilə yoxlanacaq
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle with validation."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,14 +23,16 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle with validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.width * self.height
