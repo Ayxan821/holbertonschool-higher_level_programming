60d072b47c13b91a5317b4fb131487dcdb8b07c1
Ayxan <ayxanabbas.32@example.com>
2025-06-27 12:53:01 +0000

    948

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This module defines a class MyList that inherits from list and adds a method to print the list sorted."""
+
+
 class MyList(list):
     def print_sorted(self):
         print(sorted(self))
